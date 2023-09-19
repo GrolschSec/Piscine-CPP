0cfa9e45be86326b61d15256647b30a2c2cdc7ca
Romain LOUVRIER <rlouvrie@made-f0Cr5s5.clusters.42paris.fr>
2023-09-19 12:08:18 +0200

    making private all functions that are not used by PhoneBook in Contact

--- D00/ex01/Contact.hpp
+++ D00/ex01/Contact.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rlouvrie <rlouvrie@student.42.fr >         +#+  +:+       +#+        */
+/*   By: rlouvrie <rlouvrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 14:27:11 by rlouvrie          #+#    #+#             */
-/*   Updated: 2023/08/17 14:01:04 by rlouvrie         ###   ########.fr       */
+/*   Updated: 2023/09/19 12:06:17 by rlouvrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,25 +23,25 @@ class	Contact
 		std::string	_phoneNumber;
 		std::string	_darkestSecret;
 		bool		_isSet;
-	public:
-		Contact(void);
-		~Contact(void);
+		void		set_isSet(void);
+		void		setFirstName(std::string name);
+		void		setLastName(std::string name);
+		void		setNickname(std::string name);
+		void		setPhoneNumber(std::string phone);
+		void		setDarkestSecret(std::string secret);
+		std::string	formatCol(std::string col)	const;
 		std::string	getFirstName(void)			const;
 		std::string	getLastName(void)			const;
 		std::string	getNickname(void)			const;
 		std::string	getPhoneNumber(void)		const;
 		std::string	getDarkestSecret(void)		const;
 		std::string	getInput(std::string msg)	const;
-		std::string	formatCol(std::string col)	const;
+	public:
+		Contact(void);
+		~Contact();
 		void		displayLine(int index)		const;
 		void		printContact(void)			const;
-		bool		getIsSet(void)	const;
-		void		set_isSet(void);
+		bool		getIsSet(void)				const;
 		void		init(void);
-		void		setFirstName(std::string name);
-		void		setLastName(std::string name);
-		void		setNickname(std::string name);
-		void		setPhoneNumber(std::string phone);
-		void		setDarkestSecret(std::string secret);
 };
 #endif
