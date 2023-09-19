3f2be6d5e093c9790b3e1c9ba64e8e6870086609
Romain LOUVRIER <rlouvrie@made-f0Cr5s5.clusters.42paris.fr>
2023-09-19 12:10:41 +0200

    making all functions not used by main private

--- D00/ex01/PhoneBook.hpp
+++ D00/ex01/PhoneBook.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   PhoneBook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rlouvrie <rlouvrie@student.42.fr >         +#+  +:+       +#+        */
+/*   By: rlouvrie <rlouvrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 14:27:26 by rlouvrie          #+#    #+#             */
-/*   Updated: 2023/08/14 19:39:58 by rlouvrie         ###   ########.fr       */
+/*   Updated: 2023/09/19 12:09:54 by rlouvrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ class PhoneBook
 	private:
 		Contact	contacts[8];
 		int		index;
+		void	displayHeader(void)	const;
+		void	displayAll(void)	const;
+		bool	checkInput(std::string str) const;
 	public:
 		PhoneBook();
 		void	add_contact(void);
-		void	displayHeader(void)	const;
-		void	displayAll(void)	const;
 		void	search(void)		const;
-		bool	checkInput(std::string str) const;
 };
 #endif
