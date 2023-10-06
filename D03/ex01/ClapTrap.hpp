87317a923b12a36a9333561113ffd959fc65afb9
Grolsch <grolsch-sec@protonmail.com>
2023-10-06 11:47:41 +0200

    updating claptrap hpp

--- D03/ex01/ClapTrap.hpp
+++ D03/ex01/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: rlouvrie <rlouvrie@student.42.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 13:21:05 by rlouvrie          #+#    #+#             */
-/*   Updated: 2023/08/19 15:27:51 by rlouvrie         ###   ########.fr       */
+/*   Updated: 2023/10/06 11:43:40 by rlouvrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,6 @@
 
 class ClapTrap
 {
-	protected:
-		std::string	_name;
-		unsigned int	_hitPoints;
-		unsigned int	_energyPoints;
-		unsigned int	_attackDamage;
 	public:
 		ClapTrap(void);
 		ClapTrap(std::string const &name);
@@ -30,5 +25,10 @@ class ClapTrap
 		void		attack(const std::string &target);
 		void		takeDamage(unsigned int amount);
 		void		beRepaired(unsigned int amount);
+	protected:
+		std::string	_name;
+		unsigned int	_hitPoints;
+		unsigned int	_energyPoints;
+		unsigned int	_attackDamage;
 };
 #endif
