eab103c75a57f53bb1370ad2788328be96e27805
Grolsch <grolsch-sec@protonmail.com>
2023-10-10 14:52:38 +0200

    adding one more test

--- D04/ex02/main.cpp
+++ D04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rlouvrie <rlouvrie@student.42.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/20 18:11:32 by rlouvrie          #+#    #+#             */
-/*   Updated: 2023/10/10 14:28:53 by rlouvrie         ###   ########.fr       */
+/*   Updated: 2023/10/10 14:50:06 by rlouvrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 int	main(void)
 {
+	// AAnimal test;
 	// const AAnimal* meta = new AAnimal(); // Doesn't compile
 	const AAnimal* j = new Dog();
 	const AAnimal* i = new Cat();
