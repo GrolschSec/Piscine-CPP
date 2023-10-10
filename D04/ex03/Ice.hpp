dde121da81187829b6de9870b983d9a6c6e4dc56
Grolsch <grolsch-sec@protonmail.com>
2023-10-10 15:19:48 +0200

    adding virtual to Ice

--- D04/ex03/Ice.hpp
+++ D04/ex03/Ice.hpp
@@ -6,7 +6,7 @@
 /*   By: rlouvrie <rlouvrie@student.42.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 14:43:06 by rlouvrie          #+#    #+#             */
-/*   Updated: 2023/10/07 15:12:44 by rlouvrie         ###   ########.fr       */
+/*   Updated: 2023/10/10 15:19:17 by rlouvrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ class Ice : public AMateria
 	public:
 		Ice(void);
 		Ice(Ice const &ice);
-		~Ice();
+		virtual ~Ice();
 		Ice				&operator=(Ice const &rhs);
 		virtual Ice*	clone(void)	const;
 		virtual void	use(ICharacter &target);
