50deb4e815000d739f145af0a2a090eb58f8123d
Romain LOUVRIER <rlouvrie@made-f0Cr3s1.clusters.42paris.fr>
2023-10-24 17:26:53 +0200

    adding files from ex01

---
+++ D05/ex01/Bureaucrat.cpp
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlouvrie <rlouvrie@student.42.fr >         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 09:04:02 by rlouvrie          #+#    #+#             */
+/*   Updated: 2023/10/24 10:35:39 by rlouvrie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(const std::string &name, int grade)
+	:
+	_name(name)
+{
+	if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+	else if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	else
+		this->_grade = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other)
+	: _name(other.getName()),
+	_grade(other.getGrade())
+{}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &rhs)
+{
+	if (this != &rhs)
+		this->_grade = rhs.getGrade();
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat() {}
+
+std::string	Bureaucrat::getName(void) const
+{
+	return (this->_name);
+}
+
+int	Bureaucrat::getGrade(void) const
+{
+	return (this->_grade);
+}
+
+void Bureaucrat::incrementGrade(void)
+{
+	if (this->_grade - 1 < 1)
+		throw Bureaucrat::GradeTooHighException();
+	else
+		this->_grade--;
+}
+
+void Bureaucrat::decrementGrade(void)
+{
+	if (this->_grade + 1 > 150)
+		throw Bureaucrat::GradeTooLowException();
+	else
+		this->_grade++;
+}
+
+std::ostream	&operator<<(std::ostream &os, const Bureaucrat &obj)
+{
+	os 
+		<< obj.getName() 
+		<< ", bureaucrat grade "
+		<< obj.getGrade()
+		<< ".\n";
+	return (os);
+}
---
+++ D05/ex01/Bureaucrat.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlouvrie <rlouvrie@student.42.fr >         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 09:04:06 by rlouvrie          #+#    #+#             */
+/*   Updated: 2023/10/24 10:24:55 by rlouvrie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+# include <iostream>
+# include <stdexcept>
+
+class Bureaucrat
+{
+	public:
+		Bureaucrat(const std::string &name, int grade);
+		Bureaucrat(const Bureaucrat &other);
+		Bureaucrat	&operator=(const Bureaucrat &rhs);
+		~Bureaucrat();
+		std::string	getName(void)	const;
+		int			getGrade(void)	const;
+		void		incrementGrade(void);
+		void		decrementGrade(void);
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Grade too low.\n");
+				}
+		};
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Grade too high.\n");
+				}
+		};
+	private:
+		const std::string	_name;
+		int					_grade;
+		Bureaucrat(void);
+};
+std::ostream	&operator<<(std::ostream &os, const Bureaucrat &obj);
+#endif
