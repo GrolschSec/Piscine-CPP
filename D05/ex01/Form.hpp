bf1d389ba0deab73f3c34cceca79bf033f833207
Romain LOUVRIER <rlouvrie@made-f0Cr3s1.clusters.42paris.fr>
2023-10-24 17:47:33 +0200

    debut ex01

--- D05/ex01/Form.hpp
+++ D05/ex01/Form.hpp
@@ -6,7 +6,19 @@
 /*   By: rlouvrie <rlouvrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 17:26:03 by rlouvrie          #+#    #+#             */
-/*   Updated: 2023/10/24 17:26:04 by rlouvrie         ###   ########.fr       */
+/*   Updated: 2023/10/24 17:47:20 by rlouvrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FORM_HPP
+# define FORM_HPP
+# include <iostream>
+
+class Form
+{
+	private:
+		const std::string	_name;
+		bool				_signed;
+			
+};
+#endif
