36eaf54ac9cf4bb55da88271b99eb3ceb8547e49
Grolsch <grolsch-sec@protonmail.com>
2023-10-24 10:20:02 +0200

    adding class definition

--- D05/ex00/Bureaucrat.cpp
+++ D05/ex00/Bureaucrat.cpp
@@ -6,8 +6,49 @@
 /*   By: rlouvrie <rlouvrie@student.42.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 09:04:02 by rlouvrie          #+#    #+#             */
-/*   Updated: 2023/10/24 09:26:23 by rlouvrie         ###   ########.fr       */
+/*   Updated: 2023/10/24 09:53:45 by rlouvrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(const std::string &name, int grade)
+	:
+	_name(name)
+{
+	
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other)
+	: _name(other.getName()),
+	_grade(other.getGrade())
+{}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &rhs)
+{
+	if (this != &rhs)
+		this->_grade = rhs.getGrade();
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat() {}
+
+std::string	Bureaucrat::getName(void) const
+{
+	return (this->_name);
+}
+
+int	Bureaucrat::getGrade(void) const
+{
+	return (this->_grade);
+}
+
+std::ostream	&operator<<(std::ostream &os, const Bureaucrat &obj)
+{
+	os 
+		<< obj.getName() 
+		<< ", bureaucrat grade "
+		<< obj.getGrade()
+		<< ".\n";
+	return (os);
+}
--- D05/ex00/Bureaucrat.hpp
+++ D05/ex00/Bureaucrat.hpp
@@ -6,24 +6,46 @@
 /*   By: rlouvrie <rlouvrie@student.42.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 09:04:06 by rlouvrie          #+#    #+#             */
-/*   Updated: 2023/10/24 09:09:36 by rlouvrie         ###   ########.fr       */
+/*   Updated: 2023/10/24 10:09:49 by rlouvrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUREAUCRAT_HPP
 # define BUREAUCRAT_HPP
 # include <iostream>
+# include <stdexcept>
 
 class Bureaucrat
 {
 	public:
-		Bureaucrat(std::string &name, int grade);
-		Bureaucrat(const Bureaucrat &bureaucrat);
+		Bureaucrat(const std::string &name, int grade);
+		Bureaucrat(const Bureaucrat &other);
 		Bureaucrat	&operator=(const Bureaucrat &rhs);
 		~Bureaucrat();
+		std::string	getName(void)	const;
+		int			getGrade(void)	const;
+		void		incrementGrade(int up);
+		void		decrementGrade(int down);
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Grade too low.\n");
+				}
+		};
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Grade too high.\n");
+				}
+		};
 	private:
 		const std::string	_name;
 		int					_grade;
 		Bureaucrat(void);
 };
+std::ostream	&operator<<(std::ostream &os, const Bureaucrat &obj);
 #endif
