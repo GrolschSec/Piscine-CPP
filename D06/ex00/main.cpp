754ab18933328bf9bae1968a85c4d1d113419447
rlouvrie <rlouvrie@student.42.fr>
2023-12-09 22:37:20 +0100

    main ok

---
+++ D06/ex00/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romain <romain@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/09 21:37:48 by romain            #+#    #+#             */
+/*   Updated: 2023/12/09 22:37:01 by romain           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+int	main(int argc, char **argv) {
+	if (argc != 2) {
+		std::cout
+			<< "Usage: ./convert [string]"
+			<< std::endl;
+		return (0);
+	}
+	ScalarConverter::convert(argv[1]);
+	return (0);
+}
