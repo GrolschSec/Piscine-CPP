11799069ca09babe319b78f7970e1b9e07cae771
rlouvrie <rlouvrie@student.42.fr>
2023-12-10 14:22:39 +0100

    class ok

---
+++ D06/ex00/ScalarConverter.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romain <romain@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/09 21:37:59 by romain            #+#    #+#             */
+/*   Updated: 2023/12/10 14:20:41 by romain           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+/* ************************************************************************** */
+
+ScalarConverter::~ScalarConverter() {}
+
+/* ************************************************************************** */
+
+void	ScalarConverter::convert(const std::string &literal) {
+	e_type	type = findType(literal);
+
+	switch (type) {
+		case INVALID:
+			break;
+		case CHAR:
+			convertChar(literal);
+			break;
+		case INT:
+			convertInt(literal);
+			break;
+		case FLOAT:
+			convertFloat(literal);
+			break;
+		case DOUBLE:
+			convertDouble(literal);
+			break;
+		case  P_LITERAL:
+			printPseudo(literal);
+			break;
+	}
+}
---
+++ D06/ex00/ScalarConverter.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romain <romain@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/09 21:40:51 by romain            #+#    #+#             */
+/*   Updated: 2023/12/10 14:22:06 by romain           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALAR_CONVERTER_HPP
+# define SCALAR_CONVERTER_HPP
+# include <iostream>
+# include <limits>
+
+enum e_type {
+	INVALID,
+	CHAR,
+	INT,
+	FLOAT,
+	DOUBLE,
+	P_LITERAL
+};
+
+class ScalarConverter {
+	public:
+		static void	convert(const std::string &literal);
+	private:
+		ScalarConverter(void);
+		ScalarConverter(const ScalarConverter &cp);
+		ScalarConverter	&operator=(const ScalarConverter &rhs);
+		~ScalarConverter();
+};
+
+/* ************************************************************************** */
+
+e_type	findType(const std::string &literal);
+bool	isChar(const std::string &literal);
+bool	isInt(const std::string &literal);
+bool	isFloat(const std::string &literal);
+bool	isPseudoLiteral(const std::string &literal);
+
+/* ************************************************************************** */
+
+void	printPseudo(const std::string &liberal);
+void	convertChar(const std::string &literal);
+void	convertInt(const std::string &literal);
+void	convertFloat(const std::string &literal);
+void	convertDouble(const std::string &literal);
+
+/* ************************************************************************** */
+#endif
