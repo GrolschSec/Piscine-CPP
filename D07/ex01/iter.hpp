af06bc9d75a4fa6332e9e9ecf22047f4774fbd5c
rlouvrie <rlouvrie@student.42.fr>
2023-12-13 13:24:31 +0100

    iter ok

--- D07/ex01/iter.hpp
+++ D07/ex01/iter.hpp
@@ -6,12 +6,17 @@
 /*   By: romain <romain@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 12:08:03 by romain            #+#    #+#             */
-/*   Updated: 2023/12/13 12:11:20 by romain           ###   ########.fr       */
+/*   Updated: 2023/12/13 13:24:17 by romain           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
-
+template <typename T>
+void	iter(T* array, size_t length, void (*func)(const T&)) {
+    for (size_t i = 0; i < length; ++i) {
+        func(array[i]);
+    }
+}
 #endif
