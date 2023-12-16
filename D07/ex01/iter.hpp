7578d2fd5585655bdb67518190b6248c980a55d7
rlouvrie <rlouvrie@student.42.fr>
2023-12-16 09:57:04 +0100

    ex01

--- D07/ex01/iter.hpp
+++ D07/ex01/iter.hpp
@@ -6,12 +6,13 @@
 /*   By: romain <romain@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 12:08:03 by romain            #+#    #+#             */
-/*   Updated: 2023/12/13 13:24:17 by romain           ###   ########.fr       */
+/*   Updated: 2023/12/16 09:52:29 by romain           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
+# include <iostream>
 
 template <typename T>
 void	iter(T* array, size_t length, void (*func)(const T&)) {
@@ -19,4 +20,10 @@ void	iter(T* array, size_t length, void (*func)(const T&)) {
         func(array[i]);
     }
 }
+
+template <typename T>
+void	print(T const &ref) {
+	std::cout << ref << std::endl;
+}
+
 #endif
--- D07/ex01/main.cpp
+++ D07/ex01/main.cpp
@@ -6,12 +6,29 @@
 /*   By: romain <romain@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 12:07:44 by romain            #+#    #+#             */
-/*   Updated: 2023/12/13 12:07:58 by romain           ###   ########.fr       */
+/*   Updated: 2023/12/16 09:56:01 by romain           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
 int	main(void) {
-	
+	std::string	strs[] = {
+		"Hello world",
+		"Salut",
+		"Bye",
+		"Au revoir",
+		"Thanks",
+		"Merci"
+	};
+	int	nbrs[] = {
+		56,
+		33333333,
+		32
+	};
+
+	std::cout << "Iter on an array of strings: \n";
+	::iter<std::string>(strs, 6, ::print<std::string>);
+	std::cout << "\nIter on an array of integers: \n";
+	::iter<int>(nbrs, 3, print<int>);
 }
