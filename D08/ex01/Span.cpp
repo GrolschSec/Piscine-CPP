3d505587ef2b2132cfb8548e238a51115ef7a728
rlouvrie <rlouvrie@student.42.fr>
2023-12-16 17:34:10 +0100

    need some test

---
+++ D08/ex01/Span.cpp
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romain <romain@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/16 16:42:49 by romain            #+#    #+#             */
+/*   Updated: 2023/12/16 17:33:04 by romain           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Span.hpp"
+
+/* ************************************************************************** */
+
+Span::Span(void) {}
+
+Span::Span(unsigned int N) {
+	std::cout
+		<< "[Span] - Constructor called with a size of: "
+		<< N
+		<< std::endl;
+	this->_vc.reserve(N);
+}
+
+Span::Span(Span const &cp) {
+	*this = cp;
+}
+
+Span	&Span::operator=(Span const &rhs) {
+	if (this != &rhs) {
+		this->_vc = rhs._vc;
+	}
+	return (*this);
+}
+
+Span::~Span() {}
+
+/* ************************************************************************** */
+
+void	Span::addNumber(int num) {
+	if (this->_vc.capacity() > this->_vc.size()) {
+		this->_vc.push_back(num);
+		return ;
+	}
+	throw std::overflow_error("[Span] - Vector is full.");
+}
+
+size_t	Span::longestSpan(void)		const {
+	std::vector<int>::const_iterator min;
+	std::vector<int>::const_iterator max;
+
+	if (this->_vc.size <= 1) {
+		std::logic_error("[Span] - Not enough values to find shortest span.");
+	}
+	min = std::min_element(this->_vc.begin(), this->_vc.end());
+	max = std::max_element(this->_vc.begin(), this->_vc.end());
+	return (*max - *min);
+}
+
+size_t	Span::shortestSpan(void)	const {
+	std::vector<int> vc2(this->_vc);
+
+	if (this->_vc.size <= 1) {
+		std::logic_error("[Span] - Not enough values to find shortest span.");
+	}
+	std::sort(vc2.begin(), vc2.back());
+	size_t	res = std::numeric_limits<size_t>::max();
+	for (size_t = 1; i < vc2.size(); i++) {
+		size_t span = vc2[i] - vc2[i - 1];
+		if (span < res)
+			res = span;
+	}
+	return (res);
+}
+
+void	Span::addNumbers(std::vector<int>::const_iterator it, 
+	std::vector<int>::const_iterator ite) {
+	if (ite < it) {
+		throw std::logic_error("[Span] - Begin must be smaller than end");
+	}
+	for (std::vector<int>::const_iterator it2 = it; it2 != end; it2++) {
+		addNumber(*it2);
+	}
+}
+
+/* ************************************************************************** */
---
+++ D08/ex01/Span.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romain <romain@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/16 16:42:52 by romain            #+#    #+#             */
+/*   Updated: 2023/12/16 17:33:31 by romain           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_HPP
+# define SPAN_HPP
+# include <vector>
+# include <iostream>
+# include <stdexcept>
+# include <algorithm>
+# include <limits>
+
+class Span {
+	public:
+		Span(unsigned int N);
+		Span(Span const &cp);
+		Span	&operator=(Span const &rhs);
+		~Span();
+		void	addNumber(int num);
+		size_t	shortestSpan(void)	const;
+		size_t	longestSpan(void)	const;
+		void	addNumbers(std::vector<int>::const_iterator it,
+			std::vector<int>::const_iterator ite);
+	private:
+		std::vector<int>	_vc;
+		Span(void);
+}
+#endif
