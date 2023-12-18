ffbef2e9e8b4ccfab032ad9616b06e11b0296f45
rlouvrie <rlouvrie@student.42.fr>
2023-12-18 11:12:31 +0100

    class definition ok

--- D08/ex02/MutantStack.hpp
+++ D08/ex02/MutantStack.hpp
@@ -6,11 +6,28 @@
 /*   By: romain <romain@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 19:02:01 by romain            #+#    #+#             */
-/*   Updated: 2023/12/17 12:09:08 by romain           ###   ########.fr       */
+/*   Updated: 2023/12/18 11:06:56 by romain           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
+# include <stack>
+# include <iostream>
 
+template <typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container> {
+	public:
+		typedef typename	Container::iterator iterator;
+		typedef typename	Container::const_iterator const_iterator;
+		MutantStack(void);
+		MutantStack(MutantStack const &cp);
+		MutantStack	&operator=(MutantStack const &rhs);
+		~MutantStack();
+		iterator		begin(void);
+		iterator		end(void);
+		const_iterator	begin(void)	const;
+		const_iterator	end(void)	const;
+};
+# include "MutantStack.tpp"
 #endif
