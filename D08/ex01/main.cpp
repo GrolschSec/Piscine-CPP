1d1be8348872e6bfdc6b65dc867104c7e4e5a0b3
rlouvrie <rlouvrie@student.42.fr>
2023-12-19 20:35:51 +0100

    adding more tests to main

--- D08/ex01/main.cpp
+++ D08/ex01/main.cpp
@@ -6,25 +6,42 @@
 /*   By: romain <romain@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 16:42:20 by romain            #+#    #+#             */
-/*   Updated: 2023/12/16 18:55:45 by romain           ###   ########.fr       */
+/*   Updated: 2023/12/19 20:34:25 by romain           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
-int	main(void) {
-	Span sp = Span(5);
+int main(void) {
+    Span 				sp = Span(10);
+	std::vector<int>	additionalNumbers(9, 2);
 
-	sp.addNumber(6);
-	sp.addNumber(3);
-	sp.addNumber(17);
-	sp.addNumber(9);
-	sp.addNumber(11);
+    sp.addNumber(6);
+    sp.addNumber(3);
+    sp.addNumber(17);
 
-	std::cout 
-		<< sp.shortestSpan()
-		<< "\n"
-		<< sp.longestSpan()
-		<< std::endl; 
-	return (0);
+    try {
+        sp.addNumbers(additionalNumbers.begin(), additionalNumbers.end());
+    } catch (const std::exception &e) {
+        std::cerr 
+			<< "Exception caught during addNumbers: "
+			<< e.what() 
+			<< std::endl;
+    }
+    try {
+        std::cout 
+			<< "Shortest Span: " 
+			<< sp.shortestSpan()
+			<< "\nLongest Span: "
+			<< sp.longestSpan()
+			<< std::endl;
+    } catch (const std::exception &e) {
+        std::cerr 
+			<< "Exception caught: " 
+			<< e.what()
+			<< std::endl;
+    }
+
+    return 0;
 }
+
