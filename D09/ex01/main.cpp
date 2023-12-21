8af373b4414e0a744dd1a11d9ebcbfb61081ca36
rlouvrie <rlouvrie@student.42.fr>
2023-12-21 11:17:29 +0100

    updating msg

--- D09/ex01/main.cpp
+++ D09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: romain <romain@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 21:35:07 by romain            #+#    #+#             */
-/*   Updated: 2023/12/20 22:45:18 by romain           ###   ########.fr       */
+/*   Updated: 2023/12/21 11:15:59 by romain           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char **argv) {
 		std::cerr
 			<< "Usage: "
 			<< argv[0]
-			<< " [ARGS]"
+			<< " \"ARGS\""
 			<< std::endl;
 		return (-1);
 	}
